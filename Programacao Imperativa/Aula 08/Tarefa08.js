801dc1540f2de353458b8d0aa3ffc270facad499
Manuel J. Pinero <mjpg20@gmail.com>
2021-08-25 19:29:44 -0300

    Arquivos da Discipliona Programação Imperativa

---
+++ Programacao Imperativa/Aula 08/Tarefa08.js
@@ -0,0 +1,1 @@
+function podeSubir(altura, acompanhado)
---
+++ Programacao Imperativa/operadoresLogicos.js
@@ -0,0 +1,22 @@
+// Comparação simples 
+// console.log(10 == 10)
+// console.log(10 !=15)
+
+// Comparação estricta (compara o valor e o tipo de dado também)
+
+// console.log(10==="10")
+// console.log(10!==10)
+
+// Comparadores logicos 
+// Tres tipos principais: AND (&&), todos valores devem ser verdadeiros
+
+// console.log((10>15)&&(10!=20)) //A primeira condição é falsa, a segunda verdadeira, tudo é falso
+// console.log((10<15)&&(10!=20)) //Primeira condição é verdade, segunda verdadeira, todo é verdade
+
+// OR definido como || nesse caso, pelo menos um dos enunciados deve ser verdadeiro para que todo o conjunto seja TRUE
+
+// console.log((10>15||(10!=20))) //O primeiro é falso, o segundo é verdadeiro, todo o conjunto é verdade
+// console.log((12%5==0)||(12!=12)) // ambas condições são falsas, por tanto todo o código sera falso
+
+// Operador NOT (!) nega a condição, por tanto se era TRUE vira FALSE ou viceversa
+console.log(!false) //Imprime TRUE, pois a negação da negação é uma afirmação
